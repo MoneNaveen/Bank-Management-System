5e7d92d75b180edad0b0fc306e5713bf99829a93
Mone Naveen <mone.naveen@gmail.com>
2025-03-17 21:27:46 +0530

    Updated project files

--- app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Account,Gender,STATE
-# Register your models here.
-admin.site.register(Account)
-admin.site.register(Gender)
-admin.site.register(STATE)
